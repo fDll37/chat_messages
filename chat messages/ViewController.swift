3092dc4dbc4090091ce64a8cf65bd7517355706b
fDll37 <dmendelev@yandex.ru>
2022-07-19 16:23:08 +0300

    Initial Commit

---
+++ chat messages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chat messages
+//
+//  Created by Данил Менделев on 19.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
