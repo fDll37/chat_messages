9d191a3a05fb4cd8af37184052d04a542fc10aac
fDll37 <dmendelev@yandex.ru>
2022-07-21 00:00:57 +0300

    create VC

--- chat messages/Controllers/ConversationsViewController.swift
+++ chat messages/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  chat messages
 //
 //  Created by Данил Менделев on 19.07.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
